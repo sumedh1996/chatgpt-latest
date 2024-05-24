13bbefcb4174fdf47d4856f12484ad39061f2f1c
Sumedh Raj <sumedh@watches.io>
2024-05-24 14:35:19 +0530

    Added Animated intro

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import AnimatedIntro from "@/components/AnimatedIntro";
+import {  View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+     <AnimatedIntro />
     </View>
   );
 }
